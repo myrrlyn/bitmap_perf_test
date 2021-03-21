1321dc7bc52c0489f143664efb13fc8b40fc25a1
Jorge C. Leitao <jorgecarleitao@gmail.com>
2021-03-21 08:03:45 +0000

    Added another approach.

--- benches/scalar_cmp.rs
+++ benches/scalar_cmp.rs
@@ -2,13 +2,19 @@ use bitmap::*;
 
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
-
 fn criterion_benchmark(c: &mut Criterion) {
-    let vec = (0..2049).map(|x| (x*x + x) % 10).collect::<Vec<_>>();
+    let vec = (0..2049).map(|x| (x * x + x) % 10).collect::<Vec<_>>();
 
-    c.bench_function("scalar_eq_bitmap", |b| b.iter(|| scalar_eq_bitmap(black_box(&vec), 0)));
+    c.bench_function("scalar_eq_bitmap", |b| {
+        b.iter(|| scalar_eq_bitmap(black_box(&vec), 0).len())
+    });
+    c.bench_function("scalar_eq_bitmap1", |b| {
+        b.iter(|| scalar_eq_bitmap1(black_box(&vec), 0).len())
+    });
 
-    c.bench_function("scalar_eq_bool", |b| b.iter(|| scalar_eq_bool(black_box(&vec), 0)));
+    c.bench_function("scalar_eq_bool", |b| {
+        b.iter(|| scalar_eq_bool(black_box(&vec), 0).len())
+    });
 }
 
 criterion_group!(benches, criterion_benchmark);
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use std::convert::TryInto;
+
 const BIT_MASK: [u8; 8] = [1, 2, 4, 8, 16, 32, 64, 128];
 
 /// Sets bit at position `i` in `byte` according to LSB
@@ -13,6 +15,36 @@ fn is_set(byte: u8, i: usize) -> bool {
     (byte & BIT_MASK[i]) != 0
 }
 
+pub fn scalar_eq_bitmap1(lhs: &[i32], rhs: i32) -> Vec<u8> {
+    let length = lhs.len();
+    let chunks = length / 8;
+
+    let mut buffer = vec![0u8; (length + 7) / 8];
+
+    let chunks_iter = lhs.chunks_exact(8);
+    let remainder = chunks_iter.remainder();
+
+    buffer[..chunks]
+        .iter_mut()
+        .zip(chunks_iter)
+        .for_each(|(byte, chunk)| {
+            (0..8).for_each(|i| {
+                if chunk[i] == rhs {
+                    *byte = set(*byte, i)
+                }
+            });
+        });
+
+    if !remainder.is_empty() {
+        let last = &mut buffer[chunks];
+        remainder.iter().enumerate().for_each(|(i, value)| {
+            if *value == rhs {
+                *last = set(*last, i)
+            }
+        });
+    };
+    buffer
+}
 
 pub fn scalar_eq_bitmap(lhs: &[i32], rhs: i32) -> Vec<u8> {
     let length = lhs.len();
@@ -46,21 +78,20 @@ pub fn scalar_eq_bool(lhs: &[i32], rhs: i32) -> Vec<bool> {
     lhs.iter().map(|x| *x == rhs).collect()
 }
 
-
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn bitmap_basics() {
-        let vec = (0..2049).map(|x| (x*x + x) % 10).collect::<Vec<_>>();
+        let vec = (0..2049).map(|x| (x * x + x) % 10).collect::<Vec<_>>();
         let result = scalar_eq_bitmap(&vec, 0);
 
         (0..2049).for_each(|i| {
             let byte = result[i / 8];
 
             let result = is_set(byte, i % 8);
-            let expected = (i*i + i) % 10 == 0;
+            let expected = (i * i + i) % 10 == 0;
             assert_eq!(result, expected);
         });
     }
